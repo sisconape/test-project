85e9bd3bb2a83aa7a4ee41104461bf5fca442d5e
sisconape <sisconape@gmail.com>
2017-10-01 21:45:10 +0700

    9.45

--- test.php
+++ test.php
@@ -29,14 +29,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'hello'
+				'text' => 'hello '.$replyToken
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
