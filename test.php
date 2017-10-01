467add3031808a85cb000d241a2136212035b4cd
sisconape <sisconape@gmail.com>
2017-10-01 21:52:33 +0700

    9.52

--- test.php
+++ test.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'hello'
+				'text' => 'hello'.$replyToken
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
