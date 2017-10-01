377e3cf85637d5924efadcbe960003759cbf86b8
sisconape <sisconape@gmail.com>
2017-10-01 21:13:33 +0700

    9.13

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
-<?php
-$access_token = 'oJmfE8Hb44GBIXwxzd+tjJFiuNhO4Wiz0hsE6BFpt+uoQtuE9B6wCsHyp4Kb39mXV2G7Qh/kTvjLQe62yC71sDDWMxdAHghNB7QdM1vuX6/2VVji+Y5tI8tVlmYOhx6OiVhK1O+99ce4mttQPdxumQdB04t89/1O/w1cDnyilFU=';
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+<a href="test.php">test</a>
 
-$url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, $url); 
-      // SSL USE 
-curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0); 
-curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
+</body>
+</html>
