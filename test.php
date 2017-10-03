451377caf7845b2835c83672c8153d49bf6721fc
sisconape <sisconape@gmail.com>
2017-10-03 18:43:35 +0700

    6.43

--- test.php
+++ test.php
@@ -31,30 +31,32 @@ if (!is_null($events['events'])) {
 			
 			// Make a POST Request to Messaging API to reply to sender
 			//$url = 'https://api.line.me/v2/bot/message/reply';
-			$access_token = "bc9PBjw9ey7HJGp+uA23IFC+tmHSS/LB6RqG4FZqabI+788V3BBb9hKA4PjeiEh/duOWvVp3/qd/TL7S62NTAKPqrZDKUYEN6/FKuK4ziSivX5HVN+frXgPsBj9l/jWMlOLducjNKqKODO15jqlRAwdB04t89/1O/w1cDnyilFU=";
-			$url = 'https://api.line.me/v2/bot/message/push';
-
-			$messages = [
+			$access_token ='bc9PBjw9ey7HJGp+uA23IFC+tmHSS/LB6RqG4FZqabI+788V3BBb9hKA4PjeiEh/duOWvVp3/qd/TL7S62NTAKPqrZDKUYEN6/FKuK4ziSivX5HVN+frXgPsBj9l/jWMlOLducjNKqKODO15jqlRAwdB04t89/1O/w1cDnyilFU=';
+$headers = ['Content-Type: application/json', 'Authorization: Bearer ' . $access_token];
+$userid = "U0fc91b822df6c2a9bfbc66232fbe93df";
+$url = 'https://api.line.me/v2/bot/message/push';
+$messages = [
         'type' => 'text',
         'text' => 'hello'.date("H:i:s",time()) 
       ];
-			$data = [
-				'to' => $userid,
-				'messages' => [$messages]
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$data = [
+        'to' => $userid,
+        'messages' => [$messages]                   
+    ];
+$post = json_encode($data);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+$ch = curl_init($url);
+   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
 
-			echo $result . "\r\n";
+
+
+curl_close($ch);
+echo $result."<br><br>";
 //		}
 //	}
 //}
