b9cfb8fe5b977de9ba244f89ac7f48de1cab2acd
sisconape <sisconape@gmail.com>
2017-10-03 18:57:07 +0700

    6.57

--- test.php
+++ test.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'hello'.date("H:i:s",time()+(60*60*7) 
+				'text' => 'hello'.date("H:i:s",time()+(60*60*7))
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
