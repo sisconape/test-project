770d074fee6ecafe0596ecfdbdaf6a323e28d652
sisconape <sisconape@gmail.com>
2017-10-03 18:52:10 +0700

    6.52

--- test.php
+++ test.php
@@ -11,7 +11,6 @@
 $access_token = 'oJmfE8Hb44GBIXwxzd+tjJFiuNhO4Wiz0hsE6BFpt+uoQtuE9B6wCsHyp4Kb39mXV2G7Qh/kTvjLQe62yC71sDDWMxdAHghNB7QdM1vuX6/2VVji+Y5tI8tVlmYOhx6OiVhK1O+99ce4mttQPdxumQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
-/*
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
@@ -27,11 +26,36 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			$replyuser = $event['source']['userId'];
 			// Build message to reply back
-	*/		
-			
+			$messages = [
+				'type' => 'text',
+				'text' => 'hello'.date("H:i:s",time()+(60*60*7) 
+			];
+
 			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			$access_token ='bc9PBjw9ey7HJGp+uA23IFC+tmHSS/LB6RqG4FZqabI+788V3BBb9hKA4PjeiEh/duOWvVp3/qd/TL7S62NTAKPqrZDKUYEN6/FKuK4ziSivX5HVN+frXgPsBj9l/jWMlOLducjNKqKODO15jqlRAwdB04t89/1O/w1cDnyilFU=';
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+/*
+$access_token ='bc9PBjw9ey7HJGp+uA23IFC+tmHSS/LB6RqG4FZqabI+788V3BBb9hKA4PjeiEh/duOWvVp3/qd/TL7S62NTAKPqrZDKUYEN6/FKuK4ziSivX5HVN+frXgPsBj9l/jWMlOLducjNKqKODO15jqlRAwdB04t89/1O/w1cDnyilFU=';
 $headers = ['Content-Type: application/json', 'Authorization: Bearer ' . $access_token];
 $userid = "U0fc91b822df6c2a9bfbc66232fbe93df";
 $url = 'https://api.line.me/v2/bot/message/push';
@@ -57,33 +81,6 @@ $result = curl_exec($ch);
 
 curl_close($ch);
 echo $result."<br><br>";
-//		}
-//	}
-//}
-echo "OK";
-
-/*
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$url = 'https://api.line.me/v2/bot/message/push';
-$userToken = 'Ud447347674fd962115910f0911b70e27';
-$messages = [
-				'type' => 'text',
-				'text' => 'helloasd' 
-			];
-$data = [
-				'to' => $userToken,
-				'messages' => [['type' => 'text',
-							   'text' => 'hellofinal']]
-		];
-		$post = json_encode($data);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-
 */
 ?>
 
